e5c93176bb9cfb44f3f1e32c9ff919b7caa960c8
ahmed-abouhijazi <sabir.ahmed2402@gmail.com>
2025-01-13 00:35:26 +0100

    first commit1

--- src/api/auth.js
+++ src/api/auth.js
@@ -11,7 +11,7 @@ export async function getAccessToken() {
         grant_type: 'refresh_token',
         client_id:'amzn1.application-oa2-client.e5d13cdca3744bfe93b49885bdff85b2',
         client_secret:'amzn1.oa2-cs.v1.6009e15710548ab40c615da192ed7c2a0fdeba7088ecaafec25333caa78514b5',
-        refresh_token
+        refresh_token:'Atzr|IwEBIBkf-8K5x1f617uW8ow-4f9tRXcAM__iuRUXADI6qVbtIJ3SjzoJBWuNenfjCAaWucQ_dZiyW8rZORW4dkVkA-jFvRckCZDC-ynXcrAKm7xWU2qrOIYWHnC-aUwV12srxoCnQqYHLlBUBRzqWsE4JEy1zU-cO_dEgWCMqt2w3LwbBVl6-4LonR7WCCxAPZAXTcgAhFfS7Fk7PiUYF7fPojFCIJ3dco2z1AQs61Y11dFbOTckw3n_DPd3PCFEExgwdyFeh314ox3LlD7jpvJb_zHiW1h58G9TItLUdvoH60v2UKhekQiQ6ITTswRqZy5fG33KEpq3aRxRBStpVrxlIL0yLLGh5Fg6t5XJKLi-krdEdQ'
     });
 
     try {
