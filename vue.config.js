aa661fbd156efdc63fdc84b50c29280a3bf60a55
ahmed-abouhijazi <sabir.ahmed2402@gmail.com>
2025-01-13 00:51:08 +0100

    first commit1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    devServer: {
+        proxy: {
+          '/orders': {
+            target: 'https://sellingpartnerapi-eu.amazon.com',
+            changeOrigin: true,
+            pathRewrite: {
+              '^/orders': '/orders',  // Rewrite URL if necessary
+            },
+          },
+        },
+      },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/SellingPartener/'
     : '/'
